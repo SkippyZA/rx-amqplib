745437abc3da91a3180a1fbdcd33e34079237565
Steven Inskip <inskips@gmail.com>
2016-02-18 22:50:47 +0200

    Removed commented code

--- src/examples/new_task.ts
+++ src/examples/new_task.ts
@@ -1,21 +1,3 @@
-//var amqp = require('amqplib');
-//var when = require('when');
-//
-//amqp.connect('amqp://localhost').then(function(conn) {
-//    return when(conn.createChannel().then(function(ch) {
-//        var q = 'task_queue';
-//        var ok = ch.assertQueue(q, {durable: true});
-//
-//        return ok.then(function() {
-//            var msg = process.argv.slice(2).join(' ') || "Hello World!";
-//            ch.sendToQueue(q, new Buffer(msg), {deliveryMode: true});
-//            console.log(" [x] Sent '%s'", msg);
-//            return ch.close();
-//        });
-//    })).ensure(function() { conn.close(); });
-//}).then(null, console.warn);
-
-
 import RxAmqpLib from '../rx-amqplib/RxAmqpLib';
 import RxConnection from '../rx-amqplib/RxConnection';
 import * as Rx from 'rx';
