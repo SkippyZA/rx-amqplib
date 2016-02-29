b0d6c8aebe5fd370455da7780d7f7dd541670b44
Steven Inskip <inskips@gmail.com>
2016-02-29 21:13:56 +0200

    Made it more functional

--- src/examples/receive_logs.ts
+++ src/examples/receive_logs.ts
@@ -1,6 +1,5 @@
-import RxAmqpLib from '../rx-amqplib/RxAmqpLib';
-import {Message} from 'amqplib/properties';
 import * as R from 'ramda';
+import RxAmqpLib from '../rx-amqplib/RxAmqpLib';
 import RxChannel from '../rx-amqplib/RxChannel';
 import AssertExchangeReply from '../rx-amqplib/reply/AssertExchangeReply';
 import AssertQueueReply from '../rx-amqplib/reply/AssertQueueReply';
@@ -11,19 +10,29 @@ let config = {
   host: 'amqp://localhost'
 };
 
+let channel = R.prop('channel');
 let messageContent = R.prop('content');
 let createChannel = R.invoker(0, 'createChannel');
-let assertQueue = R.invoker(2, 'assertQueue');
+let assertExchange = R.invoker(3, 'assertExchange')(config.exchange, config.exchangeType, { durable: false });
+// Having to do some serious type stuff here to make compiler happy. @TODO: Refactor
+let assertQueue = <Rx.Observable<AssertQueueReply>> <any>
+  R.compose(R.invoker(2, 'assertQueue')('', { exclusive: true }), channel);
+// @TODO: Refactor this function
+let consumeQueue = R.curry((queueName: string, options: any, queue: AssertQueueReply) =>
+  queue.channel.consume(queueName, options));
+
+// [IMPURE] Log message content from buffer to console.log
 let logMessageContent = R.compose(console.log, R.toString, messageContent);
+
 
 // Process stream
 RxAmqpLib.newConnection(config.host)
   .flatMap(createChannel)
-  .flatMap((channel: RxChannel) => channel.assertExchange(config.exchange, config.exchangeType, { durable: false }))
-  .flatMap((exchange: AssertExchangeReply) => exchange.channel.assertQueue('', { exclusive: true }))
+  .flatMap(assertExchange)
+  .flatMap(assertQueue)
   .flatMap((queue: AssertQueueReply) =>
     queue.channel
       .bindQueue(queue.queue, config.exchange, '')
-      .flatMap(() => queue.channel.consume(queue.queue, { noAck: true }))
+      .flatMap(consumeQueue(queue.queue, { noAck: true }))
   )
   .subscribe(logMessageContent, console.error);
