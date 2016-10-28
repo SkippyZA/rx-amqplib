ace25fa3182fe55879242e53d5b43fde6c4e8fd1
Nicolás Fantone <nfantone@gmail.com>
2016-10-28 21:06:17 +0100

    Remove dist and typings directories

--- dist/reply/DeleteQueueReply.d.ts
+++
@@ -1,8 +0,0 @@
-import { Replies } from 'amqplib/properties';
-import RxChannel from '../RxChannel';
-declare class DeleteQueueReply implements Replies.DeleteQueue {
-    channel: RxChannel;
-    messageCount: number;
-    constructor(channel: RxChannel, deleteQueue: Replies.DeleteQueue);
-}
-export default DeleteQueueReply;
--- dist/reply/EmptyReply.d.ts
+++
@@ -1,7 +0,0 @@
-import { Replies } from 'amqplib/properties';
-import RxChannel from '../RxChannel';
-declare class EmptyReply implements Replies.Empty {
-    channel: RxChannel;
-    constructor(channel: RxChannel);
-}
-export default EmptyReply;
